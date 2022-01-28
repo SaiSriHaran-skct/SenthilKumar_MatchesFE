0b5908a378aef1669a29dc9d2fe628660b43f965
SaiSriHaran-skct <58974853+SaiSriHaran-skct@users.noreply.github.com>
2022-01-28 19:19:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from "./Components/Footer/Footer"
 function App() {
   return (
     <div className="App">
-      <Headertop/>hi
+      <Headertop/>
       <Header />
       <Footer/>
     </div>
