a736a8c70560bca31e9b88345d9de3f81a39fa16
SaiSriHaran-skct <sai.sriharan@codingmart.com>
2022-01-28 19:35:58 +0530

    First Commit1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
-import Header from "./Components/Header/Header"
-import Headertop from "./Components/Headertop/Headertop"
-import Footer from "./Components/Footer/Footer"
+import Header from "./Components/Header/Header";
+import Headertop from "./Components/Headertop/Headertop";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <div className="App">
-      <Headertop/>
+      <Headertop />
       <Header />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
